275867475c1d2bb2a9a91b86231d1b898688c15f
Doung Kolbondith <45678351+DoungKolBondith@users.noreply.github.com>
2023-03-27 10:03:37 +0700

    update name

--- backend/frontend/src/components/Header.js
+++ backend/frontend/src/components/Header.js
@@ -17,7 +17,7 @@ function Header() {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand>SeakMey Shop</Navbar.Brand>
+                        <Navbar.Brand>BONDITH Shop</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
